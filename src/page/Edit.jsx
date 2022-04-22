ce64927e626e9ff3320ba63e01cad50cb803e8ef
Eugen <kovalchuk.eak@gmail.com>
2022-04-23 01:22:21 +0300

    all logic and routes

--- src/page/Edit.jsx
+++ src/page/Edit.jsx
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useSelector, useDispatch } from 'react-redux';
 import { addContact, editContact, getContactList } from 'redux/contactReducer';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, useParams } from 'react-router-dom';
 
 export const Edit = () => {
   const dispatch = useDispatch();
   const contactList = useSelector(getContactList);
   const location = useLocation();
-  const { id, title, placeHolderName, placeHolderEmail, placeHolderPhone } =
-    location.state;
+
+  console.log(location);
+
   let navigate = useNavigate();
 
+  const params = useParams();
+  const [fields, setFields] = useState(
+    !params?.id
+      ? {
+          id: null,
+          title: `Add Contact : `,
+          name: 'Sebastian Pereiro',
+          email: 'vasiya@rus.net',
+          phone: '000-000-0000',
+        }
+      : {
+          id: params.id,
+          title: `Edit Contact # ${
+            contactList.filter(el => el.id === params.id)[0].name
+          }`,
+          name: contactList.filter(el => el.id === params.id)[0].name,
+          email: contactList.filter(el => el.id === params.id)[0].email,
+          phone: contactList.filter(el => el.id === params.id)[0].phone,
+        }
+  );
+
+  const { id, title, name, email, phone } = fields;
   return (
     <div>
       <h1>{`${title}`}</h1>
       <Formik
         initialValues={{
-          name: id ? placeHolderName : '',
-          email: id ? placeHolderEmail : '',
-          phone: id ? placeHolderPhone : '',
+          name: name,
+          email: email,
+          phone: phone,
         }}
         validate={values => {
           const errors = {};
           if (!values.name) {
             errors.name = 'Required';
           } else if (
-            contactList.filter(({ name }) => name === values.name).length !== 0
+            contactList.filter(({ name }) => name === values.name).length !==
+              0 &&
+            id === null
           ) {
             errors.name = 'This name is already in the list';
           }
@@ -54,22 +79,19 @@ export const Edit = () => {
       >
         {({ isSubmitting }) => (
           <Form>
-            <Field type="name" name="name" placeholder={`${placeHolderName}`} />
+            <Field type="name" name="name" placeholder={`${name}`} />
             <ErrorMessage name="name" component="div" />
-            <Field
-              type="email"
-              name="email"
-              placeholder={`${placeHolderEmail}`}
-            />
+            <Field type="email" name="email" placeholder={`${email}`} />
             <ErrorMessage name="email" component="div" />
-            <Field
-              type="text"
-              name="phone"
-              placeholder={`${placeHolderPhone}`}
-            />
+            <Field type="text" name="phone" placeholder={`${phone}`} />
             <ErrorMessage name="phone" component="div" />
             <button type="submit">Submit</button>
-            <button type="button" onClick={() => navigate('/')}>
+            <button
+              type="button"
+              onClick={() =>
+                location.state?.id ? navigate(-1) : navigate('/')
+              }
+            >
               Cancel
             </button>
           </Form>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,55 +1,56 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { deleteContact, getContactList } from 'redux/contactReducer';
 
 export const Home = () => {
   const [list, setList] = useState([]);
 
+  const location = useLocation();
+  const navigate = useNavigate();
+
   const contactList = useSelector(getContactList);
   const dispatch = useDispatch();
+
+  const [query, setQuery] = useState(
+    new URLSearchParams(location.search).get('query')
+      ? new URLSearchParams(location.search).get('query')
+      : ''
+  );
 
   useEffect(() => {
     localStorage.setItem('contact', JSON.stringify(contactList));
     setList(contactList);
   }, [contactList]);
 
-  const handlerChangeQuery = query => {
-    query !== ''
-      ? setList(
-          contactList.filter(({ name }) => name.toLowerCase().includes(query))
-        )
-      : setList(contactList);
-  };
+  useEffect(() => {
+    setList(
+      contactList.filter(({ name }) => name.toLowerCase().includes(query))
+    );
+  }, [contactList, query]);
 
-  const handlerOnSubmit = e => {
-    e.preventDefault();
-    e.target[0].value = '';
+  const handleChange = el => {
+    const queryValue = el.target.value.toLowerCase().trim();
+    setQuery(queryValue);
+    location.search = queryValue;
+    location.search !== ''
+      ? navigate(`?query=${location.search}`)
+      : navigate(`/`);
   };
 
   return (
     <>
       <p>PHONE BOOK</p>
-      <Link
-        to="/edit"
-        state={{
-          title: 'Add contact',
-          placeHolderName: 'Sebastian Pereiro',
-          placeHolderEmail: 'vasiya@rus.net',
-          placeHolderPhone: '000-000-0000',
-        }}
-      >
-        Add contact...
-      </Link>
-      <form onSubmit={handlerOnSubmit}>
-        <input
-          type="text"
-          onChange={e =>
-            handlerChangeQuery(e.target.value.toLowerCase().trim())
-          }
-        />
-        <button type="submit">Find</button>
-      </form>
+      <Link to="/edit">Add contact...</Link>
+      {/* <form onSubmit={handlerOnSubmit}> */}
+      <input
+        type="text"
+        defaultValue={`${query}`}
+        onChange={handleChange}
+        placeholder="Input search name..."
+      />
+      {/* <button type="submit">Find</button> */}
+      {/* </form>  */}
       <ul>
         {list.map(({ id, name, email, phone }) => {
           return (
@@ -60,13 +61,9 @@ export const Home = () => {
                 <p>Phone: {phone}</p>
               </div>
               <Link
-                to="edit"
+                to={`edit/${id}`}
                 state={{
                   id: id,
-                  title: 'Edit contact',
-                  placeHolderName: name,
-                  placeHolderEmail: email,
-                  placeHolderPhone: phone,
                 }}
               >
                 Edit
