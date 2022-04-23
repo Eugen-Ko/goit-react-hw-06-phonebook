4a85b66a175d4982900ac511ab51843248816715
Eugen <kovalchuk.eak@gmail.com>
2022-04-24 01:55:30 +0300

    styled HomePage

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,20 @@
 import { Outlet } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
 
 export const Layout = () => {
   return (
     <>
-      <Outlet />
+      <Container
+        p={3}
+        maxW="sm"
+        bg="#fffdde"
+        centerContent
+        borderRadius="lg"
+        borderWidth="3px"
+        borderStyle="ridge"
+      >
+        <Outlet />
+      </Container>
     </>
   );
 };
