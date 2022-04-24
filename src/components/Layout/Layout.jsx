eb6278ccf6b63af793365742761194eb03ef2bba
Eugen <kovalchuk.eak@gmail.com>
2022-04-24 17:58:28 +0300

    remove some library

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,23 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { Container } from '@chakra-ui/react';
-import { Toaster } from 'react-hot-toast';
 
 export const Layout = () => {
   return (
     <>
-      {/* <Container
-        p={3}
-        maxW="sm"
-        bg="#fffdde"
-        centerContent
-        borderLeft="3px"
-        borderRight="3px"
-        borderBottom={1}
-        borderStyle="ridge"
-      > */}
       <Outlet />
-      <Toaster />
-      {/* </Container> */}
     </>
   );
 };
